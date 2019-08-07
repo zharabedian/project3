fee68aeb025149c55417117d1023a715511c89b1
Erica Walsh <erica.f.walsh@gmail.com>
2019-08-06 20:30:11 -0400

    form working and sending data back to Discover component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Discover from "./pages/Discover";
 import About from "./pages/About";
-import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -14,9 +13,8 @@ function App() {
         <Navbar />
         <Wrapper>
           <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
+          <Route exact path="/welcome" component={About} />
+          <Route exact path="/customize" component={Discover} />
         </Wrapper>
         <Footer />
       </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Pupster
+        Zel Button
       </Link>
       <div>
         <ul className="navbar-nav">
@@ -20,23 +20,15 @@ function Navbar() {
                   : "nav-link"
               }
             >
-              About
+              Welcome
             </Link>
           </li>
           <li className="nav-item">
             <Link
-              to="/discover"
-              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+              to="/customize"
+              className={window.location.pathname === "/customize" ? "nav-link active" : "nav-link"}
             >
-              Discover
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/search"
-              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-            >
-              Search
+              Customize Your Button
             </Link>
           </li>
         </ul>
