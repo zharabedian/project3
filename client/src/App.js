b8ba21db6a22662c9817791afb0f5a70b12ed93d
Erica Walsh <erica.f.walsh@gmail.com>
2019-08-08 19:58:41 -0400

    About and Books pages updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import Books from "./pages/Books";
-import Nav from "./components/Nav";
+import Books from "./pages/Books/Books";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/Books/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+// import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Books />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/welcome" component={About} />
+          <Route exact path="/customize" component={Books} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Books/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Hero from "../../components/Hero";
+import Container from "../../components/Container";
+import Row from "../../components/Row";
+import Col from "../../components/Col";
+
+function About() {
+  return (
+    <div>
+      <Hero backgroundImage="https://static.onecms.io/wp-content/uploads/sites/35/2019/06/27205558/woman-running-alone-wide.jpg">
+        <h1>Zel Button</h1>
+        <h2>Safety in any situation.</h2>
+      </Hero>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
+            <h1>Welcome To Zel Button!</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <p>
+              Here we will describe the button. 
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default About;
