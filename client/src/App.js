e6409cf52c439a3b3941ef08ada4d8c7540b41be
Erica Walsh <erica.f.walsh@gmail.com>
2019-08-10 12:22:05 -0400

    form successfully sends data to the mongoDB

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Form from "./pages/Books";
+import Form from "./pages/Form";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
--- models/book.js
+++ models/book.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  serialNumber: { type: String, required: true },
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  history: { type: String, required: true },
+  medications: { type: String, required: true },
+  allergies: { type: String, required: true },
+  eContactName: { type: String, required: true },
+  eContactPhoneNumber: { type: String, required: true },
+  eContactEmail: { type: String, required: true }
 });
 
 const Book = mongoose.model("Book", bookSchema);
